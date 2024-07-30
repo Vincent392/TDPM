6cb2d25e4f4399aecb84f99c09de0a8931776234
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-07-30 19:21:08 +0100

    Update tdpm_main.cpp

--- src/tdpm_main.cpp
+++ src/tdpm_main.cpp
@@ -4,4 +4,20 @@
  * Author(s): Vincent392
  */
 
-#include <stdio>
+#include <stdio.h>
+
+int main() {
+//Command Line Arguments, we need these
+    for (int i = 1; i < argc; ++i) {
+        if (std::strcmp(args[i], "help") == 0) {
+           showhelp()
+        } else if (std::strcmp(args[i], "install") == 0) {
+
+        } else if (std::strcmp(args[i], "uninstall") == 0) {
+
+        } else {
+         Printf("Error 01");
+         Printf("No command line argument specified\n");
+         Printf("Type \"Help\" to view them all.\n");
+         return E01;
+        }
