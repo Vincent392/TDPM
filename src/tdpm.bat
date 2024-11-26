499063793ffc518968dbedec40a0eabe7815e060
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-11-26 16:41:55 +0000

    batch re-write, yes, seriously

---
+++ src/tdpm.bat
@@ -0,0 +1,6 @@
+@Echo off
+TITLE Til Dos Package Manager (Windows)
+Echo Codename: Unnatural Selection
+:: Codename comes from the Muse Song, I was listening to it when starting this re-write
+Echo No command provided, exiting...
+EXIT
