91a98aed957fc92df62fc6b6be6973e95719d508
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-11-26 16:44:33 +0000

    echo message

--- src/tdpm.bat
+++ src/tdpm.bat
@@ -2,5 +2,6 @@
 TITLE Til Dos Package Manager (Windows)
 Echo Codename: Unnatural Selection
 :: Codename comes from the Muse Song, I was listening to it when starting this re-write
-Echo No command provided, exiting...
+Echo Error -01: No command-line argument specified\n
+Echo Type \"help\" to view available commands.
 EXIT
